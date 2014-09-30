0e0df576375796e3b45b5136e6be11fa235f336f
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2014-09-30 15:33:32 +0800

    Add bootstrap.sh script

---
+++ bin/bootstrap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd ~
+if [ ! `which git` ]; then
+  sudo apt-get install -y git
+fi
+
+if [! -d ~/dotfiles ]; then
+    git clone https://github.com/ruiwen/dotfiles.git
+fi
+
+if [ -f ~/.profile ]; then
+    mv ~/.profile ~/.profile.bak
+fi
+
+if [ -f ~/.bashrc ]; then
+    mv ~/.bashrc ~/.bashrc.bak
+fi
+
+for f in pythonrc tmux.conf bashrc dir_colors gitconfig profile; do
+    ln -s dotfiles/.${f} ~/.${f}
+done
+
+. ~/.profile
+. ~/.bashrc
+touch ~/.python_history
