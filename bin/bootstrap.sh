0fe88cc867286dc95753e8fb668e61160356ecb4
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2016-04-22 19:26:30 +0000

    Clone from SSH endpoint

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -29,7 +29,7 @@ if [ ! `which vim` ]; then
 fi
 
 if [ ! -d ~/dotfiles ]; then
-    git clone https://github.com/ruiwen/dotfiles.git ~/dotfiles
+    git clone git@github.com:ruiwen/dotfiles.git ~/dotfiles
 fi
 
 if [ -f ~/.profile ]; then
