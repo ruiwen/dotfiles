6192cb4d71920dec4f0049a5f681faa5200fc1ae
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2016-07-07 16:57:32 +0000

    Remove --config verification lines

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -37,11 +37,8 @@ if [ ! `which nvim` ]; then
   sudo apt-get install neovim python-dev python-pip python3-dev python3-pip
   sudo pip3 install neovim
   sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-  sudo update-alternatives --config vi
   sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-  sudo update-alternatives --config vim
   sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-  sudo update-alternatives --config editor
 fi
 
 if [ ! -d ~/dotfiles ]; then
