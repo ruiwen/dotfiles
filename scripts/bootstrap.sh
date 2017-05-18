91ed9a0dad76bc7e87209378d6edc947e69da0e5
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2017-05-18 13:13:30 +0000

    Chain the commands for install neovim

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -32,12 +32,12 @@ if [ ! -z ${APPS} ]; then
 fi
 
 if [ ! `which nvim` ]; then
-  sudo add-apt-repository ppa:neovim-ppa/unstable
-  sudo apt-get update
-  sudo apt-get install neovim python3-jedi python-dev python-pip python3-dev python3-pip
-  sudo pip3 install neovim
-  sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-  sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+  sudo add-apt-repository ppa:neovim-ppa/unstable &&
+  sudo apt-get update &&
+  sudo apt-get install neovim python3-jedi python-dev python-pip python3-dev python3-pip &&
+  sudo pip3 install -U neovim &&
+  sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60 &&
+  sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60 &&
   sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 fi
 
